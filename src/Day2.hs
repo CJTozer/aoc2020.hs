3fa107098b862e42607b7dcffd6c4d4e0f54c57f
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-11 18:16:58 +0100

    Day2 tests pass

--- src/Day2.hs
+++ src/Day2.hs
@@ -15,4 +15,23 @@ day2 = do
 intCode :: String -> [Int]
 intCode s = do
   let ints = map read $ splitOn "," s
-  ints
+  processCmds 0 ints
+
+processCmds :: Int -> [Int] -> [Int]
+processCmds pos ints = do
+  let cmd = take 4 $ drop pos ints
+  case head cmd of
+    99 -> ints -- Program termination
+    _ -> processCmds (pos + 4) $ doCmd (cmd !! 0, cmd !! 1, cmd !! 2, cmd !! 3) ints
+
+-- operate on them - new function taking 4, returning (pos, value)
+-- create a new array with the replaced value
+doCmd :: (Int, Int, Int, Int) -> [Int] -> [Int]
+doCmd (op, x_pos, y_pos, o_pos) ints = do
+  -- Addition
+  let (as, bs) = splitAt o_pos ints
+  let new_val = case op of
+        1 -> ints !! x_pos + ints !! y_pos
+        2 -> ints !! x_pos * ints !! y_pos
+        _ -> -1 -- Illegal opcode!
+  as ++ [new_val] ++ tail bs
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -10,7 +10,7 @@ test_day2 = hspec $ do
       intCode "1,0,0,0,99" `shouldBe` [2,0,0,0,99]
 
     it "2,3,0,3,99" $ do
-      intCode "1,0,0,0,99" `shouldBe` [2,3,0,6,99]
+      intCode "2,3,0,3,99" `shouldBe` [2,3,0,6,99]
 
     it "2,4,4,5,99,0" $ do
       intCode "2,4,4,5,99,0" `shouldBe` [2,4,4,5,99,9801]
