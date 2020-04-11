552856b8da6cfcbde414b9cac4f0cc7b0f7c5a85
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-11 13:50:21 +0100

    Take in parameter for test to run

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import System.Environment
 import Lib
 
 main :: IO ()
-main = someFunc
+main = getArgs >>= runTest
+
+runTest :: [String] -> IO ()
+runTest ["1"] = someFunc
+runTest _ = putStrLn "Please provide the scenario to run"
