aa0ad544b5382d6cf72a52326c7dd4aeeaec4e83
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-11 16:55:48 +0100

    Unit testing for day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,6 +1,19 @@
-module Day1
-    ( day1
-    ) where
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Day1 where
 
 day1 :: IO ()
-day1 = putStrLn "day1"
+day1 = do
+  putStrLn "day1 start"
+  contents <- readFile "data/day1"
+  let ints :: [Int] = map read $ lines contents
+  print $ show $ fuelNeeded ints
+  putStrLn "day1 end"
+
+fuelNeeded :: [Int] -> Int
+fuelNeeded xs = sum $ map fuelForModule xs
+
+fuelForModule :: Int -> Int
+fuelForModule x
+  | x <= 6 = 0
+  | otherwise = x `div` 3 - 2
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+import Test.Hspec
+import Day1
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "fuelForModule" $ do
+    it "returns 0 for 0 input" $ do
+      fuelForModule 0 `shouldBe` 0
+
+    it "returns 0 for 6 input" $ do
+      fuelForModule 6 `shouldBe` 0
+
+    it "returns 2 for 12 input" $ do
+      fuelForModule 12 `shouldBe` 2
+
+    it "returns 2 for 14 input" $ do
+      fuelForModule 14 `shouldBe` 2
+
+    it "returns 654 for 1969 input" $ do
+      fuelForModule 1969 `shouldBe` 654
+
+    it "returns 33583 for 100756 input" $ do
+      fuelForModule 100756 `shouldBe` 33583
+
+  describe "fuelNeeded" $ do
+    it "returns 656 for [7, 14, 1969] input" $ do
+      fuelNeeded [7, 14, 1969] `shouldBe` 656
