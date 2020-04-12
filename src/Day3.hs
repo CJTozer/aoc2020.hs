b5d801ed851ac00f82f46d1528b3cf3f47d5c199
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-12 17:51:02 +0100

    Refactoring to collect points along track

--- src/Day3.hs
+++ src/Day3.hs
@@ -12,10 +12,24 @@ day3 = do
   contents <- readFile "data/day3"
   let ls = lines contents
   let w1:w2:_ = ls
-  let closest = closestIntersection (parseWire w1) (parseWire w2)
+  let (w1_tracks, w1_ps) = parseWire w1
+  let (w2_tracks, w2_ps) = parseWire w2
+  let closest = closestIntersection w1_tracks w2_tracks
   print (show closest)
   putStrLn "day2 end"
 
+-- Get the intersection with the shortest delay
+fastestIntersection :: WireTracks -> WireTracks -> Int
+fastestIntersection (h1, v1) (h2, v2) = do
+  let intersections = (allIntersections h1 v2) ++ (allIntersections h2 v1)
+-- TODO
+  let manhattan_ds = sort [(abs x) + (abs y) | (x, y) <- intersections]
+  head (tail manhattan_ds)
+
+-- distanceToIntersection
+-- TODO
+
+-- Get the intersection with the closest (Manhattan) distance to the origin
 closestIntersection :: WireTracks -> WireTracks -> Int
 closestIntersection (h1, v1) (h2, v2) = do
   let intersections = (allIntersections h1 v2) ++ (allIntersections h2 v1)
@@ -26,7 +40,7 @@ data Hor = Hor { x1::Int, x2::Int, yy::Int } deriving (Show, Eq)
 data Ver = Ver { xx::Int, y1::Int, y2::Int } deriving (Show, Eq)
 type Point = (Int, Int)
 type WireTracks = ([Hor], [Ver])
-type WireState = ([String], Point, WireTracks)
+type WireState = ([String], [Point], WireTracks)
 
 -- Inclusive between returns (v1 <= x <= v2) - or v1/2 the other way around
 between :: Int -> Int -> Int -> Bool
@@ -46,32 +60,33 @@ intersection h v = do
     False -> Nothing
 
 -- Parse a wire's tracks from the string describing it
-parseWire :: String -> WireTracks
+parseWire :: String -> (WireTracks, [Point])
 parseWire s = do
   let origin::Point = (0, 0)
   let instructions = splitOn "," s
-  let (_, _, tracks) = collectInstructions (instructions, origin, ([], []))
-  tracks
+  let (_, points, tracks) = collectInstructions (instructions, origin:[], ([], []))
+  (tracks, reverse points)
 
 -- Read off the next instruction, collecting the Hor/Ver into the lists
 collectInstructions :: WireState -> WireState
-collectInstructions ([], p, tracks) = ([], p, tracks)
+collectInstructions ([], ps, tracks) = ([], ps, tracks)
 collectInstructions old_state = do
-  let (op:rem_ops, (x, y), (h_tracks, v_tracks)) = old_state
+  let (op:rem_ops, ps, (h_tracks, v_tracks)) = old_state
+  let (x, y) = head ps
   case head op of
     'U' -> do
       let new_y::Int = y + read (tail op)
       let new_v_tracks = (Ver x y new_y) : v_tracks
-      collectInstructions (rem_ops, (x, new_y), (h_tracks, new_v_tracks))
+      collectInstructions (rem_ops, (x, new_y):ps, (h_tracks, new_v_tracks))
     'D' -> do
       let new_y::Int = y - read (tail op)
       let new_v_tracks = (Ver x y new_y) : v_tracks
-      collectInstructions (rem_ops, (x, new_y), (h_tracks, new_v_tracks))
+      collectInstructions (rem_ops, (x, new_y):ps, (h_tracks, new_v_tracks))
     'R' -> do
       let new_x::Int = x + read (tail op)
       let new_h_tracks = (Hor x new_x y) : h_tracks
-      collectInstructions (rem_ops, (new_x, y), (new_h_tracks, v_tracks))
+      collectInstructions (rem_ops, (new_x, y):ps, (new_h_tracks, v_tracks))
     'L' -> do
       let new_x::Int = x - read (tail op)
       let new_h_tracks = (Hor x new_x y) : h_tracks
-      collectInstructions (rem_ops, (new_x, y), (new_h_tracks, v_tracks))
+      collectInstructions (rem_ops, (new_x, y):ps, (new_h_tracks, v_tracks))
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -20,20 +20,20 @@ test_day3 = hspec $ do
 
   describe "collectInstructions" $ do
     it "U2" $ do
-      let state = collectInstructions (["U2"], (0, 0), ([], []))
-      state `shouldBe` ([], (0, 2), ([], [Ver 0 0 2]))
+      let state = collectInstructions (["U2"], [(0, 0)], ([], []))
+      state `shouldBe` ([], [(0, 2),(0, 0)], ([], [Ver 0 0 2]))
 
     it "U2 R4" $ do
-      let state = collectInstructions (["U2", "R4"], (0, 0), ([], []))
-      state `shouldBe` ([], (4, 2), ([Hor 0 4 2], [Ver 0 0 2]))
+      let state = collectInstructions (["U2", "R4"], [(0, 0)], ([], []))
+      state `shouldBe` ([], [(4, 2),(0, 2),(0, 0)], ([Hor 0 4 2], [Ver 0 0 2]))
 
     it "U2 R4 D30" $ do
-      let state = collectInstructions (["U2", "R4", "D30"], (0, 0), ([], []))
-      state `shouldBe` ([], (4, -28), ([Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2]))
+      let state = collectInstructions (["U2", "R4", "D30"], [(0, 0)], ([], []))
+      state `shouldBe` ([], [(4, -28),(4, 2),(0, 2),(0, 0)], ([Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2]))
 
     it "U2 R4 D30 L6" $ do
-      let state = collectInstructions (["U2", "R4", "D30", "L6"], (0, 0), ([], []))
-      state `shouldBe` ([], (-2, -28), ([Hor 4 (-2) (-28), Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2]))
+      let state = collectInstructions (["U2", "R4", "D30", "L6"], [(0, 0)], ([], []))
+      state `shouldBe` ([], [(-2, -28),(4, -28),(4, 2),(0, 2),(0, 0)], ([Hor 4 (-2) (-28), Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2]))
 
   describe "intersection" $ do
     it "no intersection" $ do
@@ -50,23 +50,29 @@ test_day3 = hspec $ do
 
   describe "parseWire" $ do
     it "U2,R4,D30,L6" $ do
-      parseWire "U2,R4,D30,L6" `shouldBe` ([Hor 4 (-2) (-28), Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2])
+      let (ts, ps) = parseWire "U2,R4,D30,L6"
+      ts `shouldBe` ([Hor 4 (-2) (-28), Hor 0 4 2], [Ver 4 2 (-28), Ver 0 0 2])
+      ps `shouldBe` [(0, 0), (0, 2), (4, 2), (4, (-28)), ((-2), (-28))]
 
   describe "allIntersections" $ do
     it "U2,R4,D30,L6 / R6,D3,L4,U6" $ do
-      let (h1, v1) = parseWire "U2,R4,D30,L6"
-      let (h2, v2) = parseWire "R6,D3,L4,U6"
+      let ((h1, v1), _) = parseWire "U2,R4,D30,L6"
+      let ((h2, v2), _) = parseWire "R6,D3,L4,U6"
       allIntersections h1 v2 `shouldBe` [(2,2)]
       allIntersections h2 v1 `shouldBe` [(4,-3),(4,0),(0,0)]
 
   describe "closestIntersection" $ do
     it "U2,R4,D30,L6 / R6,D3,L4,U6" $ do
-      let w1 = parseWire "U2,R4,D30,L6"
-      let w2 = parseWire "R6,D3,L4,U6"
+      let (w1, _) = parseWire "U2,R4,D30,L6"
+      let (w2, _) = parseWire "R6,D3,L4,U6"
       closestIntersection w1 w2 `shouldBe` 4
 
     it "Day 3 Example 1" $ do
-      closestIntersection (parseWire "R75,D30,R83,U83,L12,D49,R71,U7,L72") (parseWire "U62,R66,U55,R34,D71,R55,D58,R83") `shouldBe` 159
+      let (t1, _) = parseWire "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+      let (t2, _) = parseWire "U62,R66,U55,R34,D71,R55,D58,R83"
+      closestIntersection t1 t2 `shouldBe` 159
 
     it "Day 3 Example 2" $ do
-      closestIntersection (parseWire "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51") (parseWire "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") `shouldBe` 135
+      let (t1, _) = parseWire "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+      let (t2, _) = parseWire "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+      closestIntersection t1 t2 `shouldBe` 135
