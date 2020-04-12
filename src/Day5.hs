1cbcc02df7186326bb9328cccbd174b96b8a37f2
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-13 00:39:14 +0100

    Get pointers for direct/indirect input

--- src/Day5.hs
+++ src/Day5.hs
@@ -3,9 +3,11 @@
 module Day5 where
 
 import Data.List.Split
+import Debug.Trace
 
 type IPtr = Int -- Instruction Pointer
 type PState = [Int] -- Program state
+type OpCode = Int
 
 day5 :: IO ()
 day5 = do
@@ -23,7 +25,7 @@ runIntCode s = do
 runIntCodeFrom :: IPtr -> PState -> PState
 runIntCodeFrom pos ints = do
   let opcode = ints !! pos
-  case opcode of
+  case rem opcode 100 of
     1 -> runIntCodeFrom (pos + 4) $ opAdd pos ints
     2 -> runIntCodeFrom (pos + 4) $ opMultiply pos ints
     3 -> runIntCodeFrom (pos + 2) $ opInput pos ints
@@ -41,9 +43,12 @@ opMultiply = opMath (*)
 -- Generic maths
 opMath :: (Int -> Int -> Int) -> IPtr -> PState -> PState
 opMath op pos state = do
-  let ptr_a = state !! (pos + 1)
-  let ptr_b = state !! (pos + 2)
-  let ptr_out = state !! (pos + 3)
+  let flags = div (state !! pos) 100
+  let ptrs = getPointers flags 3 (pos + 1) state
+  let ptr_a = ptrs !! 0
+  let ptr_b = ptrs !! 1
+  let ptr_out = ptrs !! 2
+
   let a = state !! ptr_a
   let b = state !! ptr_b
   updateValue ptr_out (op a b) state
@@ -53,19 +58,26 @@ opMath op pos state = do
 opInput :: IPtr -> PState -> PState
 opInput pos state = do
   let ptr_out = state !! (pos + 1)
-  let value = 1 -- TODO get actual input!
+  let value = trace ("+++ Setting input to 1 for ptr " ++ show ptr_out) 1 -- TODO get actual input!
   updateValue ptr_out value state
 
 -- Output operation
 opOutput :: IPtr -> PState -> PState
 opOutput pos state = do
   let ptr_out = state !! (pos + 1)
-  let value = state !! ptr_out
-  -- TODO print
-  state
+  trace ("+++ Value at pointer " ++ (show ptr_out) ++ " is " ++ (show (state !! ptr_out))) state
 
 -- Get a new state with a single uipdated value
 updateValue :: IPtr -> Int -> PState -> PState
 updateValue pos value state = do
   let (as, bs) = splitAt pos state
   as ++ [value] ++ tail bs
+
+-- Get pointer values from an opcode and subsequent values
+getPointers :: Int -> Int -> IPtr -> PState -> [IPtr]
+getPointers _ 0 _ _ = []
+getPointers flags num pos state = do
+  let ptr = case rem flags 10 of
+        0 -> state !! pos
+        _ -> pos
+  ptr : (getPointers (div flags 10) (num - 1) (pos + 1) state)
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -23,3 +23,22 @@ test_day5 = hspec $ do
 
     it "Test output: 1,0,0,0,4,0,99" $ do
       runIntCode "1,0,0,0,4,0,99" `shouldBe` [2,0,0,0,4,0,99]
+
+    it "Test direct params - website example" $ do
+      runIntCode "1002,4,3,4,33" `shouldBe` [1002,4,3,4,99]
+
+    it "Test direct params - website example 2" $ do
+      runIntCode "1101,100,-1,4,0" `shouldBe` [1101,100,-1,4,99]
+
+  describe "getPointers" $ do
+    it "All indirect" $ do
+      getPointers 0 2 0 [1,2] `shouldBe` [1,2]
+
+    it "All direct" $ do
+      getPointers 11 2 0 [1,0] `shouldBe` [0,1]
+
+    it "Overflow defaults to indirect" $ do
+      getPointers 1 2 0 [1,0] `shouldBe` [0,0]
+
+    it "A mixture" $ do
+      getPointers 10101 6 0 [1,0,5,2,3,4] `shouldBe` [0,0,2,2,4,4]
