92c18a087c37693607b453bac7bdb5299cc9201c
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-13 18:33:38 +0100

    runIntCodeWithInputs, and tests

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Day7 where
+module Day7 (day7, runIntCodeWithInputs) where
 
 import Data.List.Split
 import Debug.Trace
@@ -9,4 +9,151 @@ day7 :: IO ()
 day7 = do
   putStrLn "day7 start"
   contents <- readFile "data/day7"
+  let final_state = runIntCodeWithInputs contents []
+  print (show final_state)
   putStrLn "day7 end"
+
+type IPtr = Int -- Instruction Pointer
+type PState = ([Int], IOValues) -- Program state
+type IOValues = ([Int], [Int]) -- Program inputs & outputs
+
+runIntCodeWithInputs :: String -> [Int] -> PState
+runIntCodeWithInputs s inputs = do
+  let ints = map read $ splitOn "," s
+  runIntCodeFrom 0 (ints, (inputs, []))
+
+runIntCodeFrom :: IPtr -> PState -> PState
+runIntCodeFrom pos state = do
+  let (ints, _) = state
+  let opcode = ints !! pos
+  case rem opcode 100 of
+    1 -> runIntCodeFrom (pos + 4) $ opAdd pos state
+    2 -> runIntCodeFrom (pos + 4) $ opMultiply pos state
+    3 -> runIntCodeFrom (pos + 2) $ opInput pos state
+    4 -> runIntCodeFrom (pos + 2) $ opOutput pos state
+    5 -> do
+      let new_pos = opJumpIfTrue pos state
+      runIntCodeFrom new_pos state
+    6 -> do
+      let new_pos = opJumpIfFalse pos state
+      runIntCodeFrom new_pos state
+    7 -> runIntCodeFrom (pos + 4) $ opLessThan pos state
+    8 -> runIntCodeFrom (pos + 4) $ opEquals pos state
+    99 -> state
+    _ -> trace ("!!! Unexpected op code " ++ (show opcode) ++ " at pointer " ++ (show pos)) undefined
+
+-- Addition operation
+opAdd :: IPtr -> PState -> PState
+opAdd = opMath (+)
+
+-- Multiplication operation
+opMultiply :: IPtr -> PState -> PState
+opMultiply = opMath (*)
+
+-- Generic maths
+opMath :: (Int -> Int -> Int) -> IPtr -> PState -> PState
+opMath op pos state = do
+  let (ints, ios) = state
+  let ptrs = getPointersFromOpPtr 3 pos ints
+  let ptr_a = ptrs !! 0
+  let ptr_b = ptrs !! 1
+  let ptr_out = ptrs !! 2
+
+  let a = ints !! ptr_a
+  let b = ints !! ptr_b
+  (updateValue ptr_out (op a b) ints, ios)
+
+-- Input operation
+-- Cheating for now by knowing the input should be 1
+opInput :: IPtr -> PState -> PState
+opInput pos state = do
+  let (ints, (ins, outs)) = state
+  let ptrs = getPointersFromOpPtr 1 pos ints
+  let ptr_out = ptrs !! 0
+  let value:rem_ins = ins
+  let new_ios = trace ("+++ Using input " ++ (show value) ++ " remaining inputs: " ++ (show rem_ins)) (rem_ins, outs)
+  (updateValue ptr_out value ints, new_ios)
+
+-- Output operation
+opOutput :: IPtr -> PState -> PState
+opOutput pos state = do
+  let (ints, (ins, outs)) = state
+  let ptrs = getPointersFromOpPtr 1 pos ints
+  let ptr_out = ptrs !! 0
+  let new_outs = outs ++ [(ints !! ptr_out)]
+  trace ("+++ Outputs now: " ++ (show new_outs)) (ints, (ins, new_outs))
+
+-- Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+opJumpIfTrue :: IPtr -> PState -> IPtr
+opJumpIfTrue pos state = do
+  let (ints, _) = state
+  let ptrs = getPointersFromOpPtr 2 pos ints
+  let ptr_check = ptrs !! 0
+  let ptr_new_pos = ptrs !! 1
+  case ints !! ptr_check of
+    0 -> pos + 3
+    _ -> ints !! ptr_new_pos
+
+-- Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+opJumpIfFalse :: IPtr -> PState -> IPtr
+opJumpIfFalse pos state = do
+  let (ints, _) = state
+  let ptrs = getPointersFromOpPtr 2 pos ints
+  let ptr_check = ptrs !! 0
+  let ptr_new_pos = ptrs !! 1
+  case ints !! ptr_check of
+    0 -> ints !! ptr_new_pos
+    _ -> pos + 3
+
+-- Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+opLessThan :: IPtr -> PState -> PState
+opLessThan pos state = do
+  let (ints, ios) = state
+  let ptrs = getPointersFromOpPtr 3 pos ints
+  let ptr_a = ptrs !! 0
+  let ptr_b = ptrs !! 1
+  let ptr_out = ptrs !! 2
+
+  -- Get the values to compare
+  let a = ints !! ptr_a
+  let b = ints !! ptr_b
+  case a < b of
+    True -> (updateValue ptr_out 1 ints, ios)
+    False -> (updateValue ptr_out 0 ints, ios)
+
+-- Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+opEquals :: IPtr -> PState -> PState
+opEquals pos state = do
+  let (ints, ios) = state
+  let ptrs = getPointersFromOpPtr 3 pos ints
+  let ptr_a = ptrs !! 0
+  let ptr_b = ptrs !! 1
+  let ptr_out = ptrs !! 2
+
+  -- Get the values to compare
+  let a = ints !! ptr_a
+  let b = ints !! ptr_b
+  case a == b of
+    True -> (updateValue ptr_out 1 ints, ios)
+    False -> (updateValue ptr_out 0 ints, ios)
+
+-- Get a new state with a single uipdated value
+updateValue :: IPtr -> Int -> [Int] -> [Int]
+updateValue pos value ints = do
+  let (as, bs) = splitAt pos ints
+  as ++ [value] ++ tail bs
+
+-- Get parameter pointers from instruction pointer
+getPointersFromOpPtr :: Int -> IPtr -> [Int] -> [IPtr]
+getPointersFromOpPtr num pos ints = do
+  let flags = div (ints !! pos) 100
+  getPointers flags num (pos + 1) ints
+
+-- Get pointer values from flags and position of first parameter
+getPointers :: Int -> Int -> IPtr -> [Int] -> [IPtr]
+getPointers _ 0 _ _ = []
+getPointers flags num pos ints = do
+  let ptr = case rem flags 10 of
+        0 -> ints !! pos
+        _ -> pos
+  ptr : (getPointers (div flags 10) (num - 1) (pos + 1) ints)
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -5,6 +5,78 @@ import Day7
 
 test_day7 :: IO ()
 test_day7 = hspec $ do
-  describe "placeholder" $ do
-    it "1 == 1" $ do
-      1 `shouldBe` 1
+  describe "runIntCodeWithInputs" $ do
+    it "1,0,0,0,99" $ do
+      runIntCodeWithInputs "1,0,0,0,99" [] `shouldBe` ([2,0,0,0,99], ([], []))
+
+    it "2,3,0,3,99" $ do
+      runIntCodeWithInputs "2,3,0,3,99" [] `shouldBe` ([2,3,0,6,99], ([], []))
+
+    it "2,4,4,5,99,0" $ do
+      runIntCodeWithInputs "2,4,4,5,99,0" [] `shouldBe` ([2,4,4,5,99,9801], ([], []))
+
+    it "1,1,1,4,99,5,6,0,99" $ do
+      runIntCodeWithInputs "1,1,1,4,99,5,6,0,99" [] `shouldBe` ([30,1,1,4,2,5,6,0,99], ([], []))
+
+    it "Test input: 3,3,99,0" $ do
+      runIntCodeWithInputs "3,3,99,0" [5] `shouldBe` ([3,3,99,5], ([], []))
+
+    it "Test output: 1,0,0,0,4,0,99" $ do
+      runIntCodeWithInputs "1,0,0,0,4,0,99" [] `shouldBe` ([2,0,0,0,4,0,99], ([], [2]))
+
+    it "Test direct params - website example" $ do
+      runIntCodeWithInputs "1002,4,3,4,33" [] `shouldBe` ([1002,4,3,4,99], ([], []))
+
+    it "Test direct params - website example 2" $ do
+      runIntCodeWithInputs "1101,100,-1,4,0" [] `shouldBe` ([1101,100,-1,4,99], ([], []))
+
+    it "Test JumpIfTrue - direct params" $ do
+      runIntCodeWithInputs "1105,1,1,0,6,99,0" [] `shouldBe` ([1105,1,1,0,6,99,1106], ([], []))
+
+    it "Test JumpIfTrue - indirect params" $ do
+      runIntCodeWithInputs "5,2,6,0,6,99,1" [] `shouldBe` ([5,2,6,0,6,99,5], ([], []))
+
+    it "Test JumpIfTrue - no jump" $ do
+      runIntCodeWithInputs "1105,0,1,99" [] `shouldBe` ([1105,0,1,99], ([], []))
+
+    it "Test JumpIfFalse - direct params" $ do
+      runIntCodeWithInputs "1106,0,2,1,6,7,99,0" [] `shouldBe` ([1106,0,2,1,6,7,99,0], ([], []))
+
+    it "Test JumpIfFalse - indirect params" $ do
+      runIntCodeWithInputs "6,6,4,99,1,6,0,9,99,0" [] `shouldBe` ([6,6,4,99,1,6,0,9,99,6], ([], []))
+
+    it "Test JumpIfFalse - no jump" $ do
+      runIntCodeWithInputs "1006,0,1,99" [] `shouldBe` ([1006,0,1,99], ([], []))
+
+    it "Test LessThan - a < b" $ do
+      runIntCodeWithInputs "1107,0,1,5,99,8" [] `shouldBe` ([1107,0,1,5,99,1], ([], []))
+
+    it "Test LessThan - a = b" $ do
+      runIntCodeWithInputs "1107,1,1,5,99,8" [] `shouldBe` ([1107,1,1,5,99,0], ([], []))
+
+    it "Test LessThan - a > b" $ do
+      runIntCodeWithInputs "1107,2,1,5,99,8" [] `shouldBe` ([1107,2,1,5,99,0], ([], []))
+
+    it "Test LessThan - a < b, indirect" $ do
+      runIntCodeWithInputs "7,0,4,5,99,8" [] `shouldBe` ([7,0,4,5,99,1], ([], []))
+
+    it "Test Equals - a < b" $ do
+      runIntCodeWithInputs "1108,0,1,5,99,8" [] `shouldBe` ([1108,0,1,5,99,0], ([], []))
+
+    it "Test Equals - a = b" $ do
+      runIntCodeWithInputs "1108,1,1,5,99,8" [] `shouldBe` ([1108,1,1,5,99,1], ([], []))
+
+    it "Test Equals - a > b" $ do
+      runIntCodeWithInputs "1108,2,1,5,99,8" [] `shouldBe` ([1108,2,1,5,99,0], ([], []))
+
+    it "Test Equals - a = b, indirect" $ do
+      runIntCodeWithInputs "8,0,5,5,99,8" [] `shouldBe` ([8,0,5,5,99,1], ([], []))
+
+    it "Test multiple inputs" $ do
+      runIntCodeWithInputs "3,2,0,4,0" [3,99] `shouldBe` ([3,2,3,4,99], ([], []))
+
+    it "Test multiple inputs with direct" $ do
+      runIntCodeWithInputs "103,2,3,4,0" [93,99] `shouldBe` ([103,93,3,4,99], ([], []))
+
+    it "Test multiple outputs" $ do
+      runIntCodeWithInputs "4,0,104,912,99" [] `shouldBe` ([4,0,104,912,99], ([], [4,912]))
