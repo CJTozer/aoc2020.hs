3176471181a35da2c7f8376bfdee98e84a73ecd7
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-13 17:47:36 +0100

    Template for Day7

---
+++ src/Day7.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Day7 where
+
+import Data.List.Split
+import Debug.Trace
+
+day7 :: IO ()
+day7 = do
+  putStrLn "day7 start"
+  contents <- readFile "data/day7"
+  putStrLn "day7 end"
---
+++ test/Day7Spec.hs
@@ -0,0 +1,10 @@
+module Day7Spec where
+
+import Test.Hspec
+import Day7
+
+test_day7 :: IO ()
+test_day7 = hspec $ do
+  describe "placeholder" $ do
+    it "1 == 1" $ do
+      1 `shouldBe` 1
