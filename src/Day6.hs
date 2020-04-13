0c1e72d90db8f80a80fb15fe1b8aafa79e8ff664
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-13 14:33:50 +0100

    findElementById and findParentForId plus tests

--- src/Day6.hs
+++ src/Day6.hs
@@ -3,6 +3,7 @@
 module Day6 where
 
 import Data.List.Split
+import Data.Maybe
 import Debug.Trace
 
 day6 :: IO ()
@@ -10,32 +11,66 @@ day6 = do
   putStrLn "day6 start"
   contents <- readFile "data/day6"
   let map = constructMap contents
-  print (show map)
+  let orbits = countOrbits map
+  print (show orbits)
   putStrLn "day6 end"
 
-type OrbitMap = OrbitElement
-data OrbitElement = OrbitElement { id :: String
-                                 -- , parent :: Maybe OrbitElement -- TODO Not implemented, not sure if we'll need it
+data OrbitElement = OrbitElement { elem_id :: String
                                  , children :: [OrbitElement]
+                                 , elem_depth :: Int
                                  } deriving (Show, Eq)
 type OrbitInstruction = (String, String) -- (parent, child)
 
-constructMap :: String -> OrbitMap
+constructMap :: String -> OrbitElement
 constructMap s = do
   -- Split string into lines
   let ls = lines s
   -- let pairs = [(splitOn ")" l) | l <- ls]
   let pairs = map (splitOn ")") ls
   let ins = [(p!!0, p!!1) | p <- pairs]
-  OrbitElement "COM" (getOrbitChildren "COM" ins)
+  OrbitElement "COM" (getOrbitChildren "COM" 1 ins) 0
 
-getOrbitChildren :: String -> [OrbitInstruction] -> [OrbitElement]
-getOrbitChildren id ins = do
+getOrbitChildren :: String -> Int -> [OrbitInstruction] -> [OrbitElement]
+getOrbitChildren id depth ins = do
   let direct = filter (isInstructionFor id) ins
-  [ OrbitElement child_id (getOrbitChildren child_id ins)
+  [ OrbitElement child_id (getOrbitChildren child_id (depth + 1) ins) depth
     | (_, child_id) <- direct ]
 
 isInstructionFor :: String -> OrbitInstruction -> Bool
 isInstructionFor id ins = do
   let (parent_id, _) = ins
   parent_id == id
+
+countOrbits :: OrbitElement -> Int
+countOrbits elem = (elem_depth elem) + sum (map countOrbits (children elem))
+
+calculateManevuers :: OrbitElement -> Int
+calculateManevuers map = do
+  -- Find YOU and SAN
+  -- If not same depth, move lower up parents until matching
+  -- If same depth and same parent - bingpot
+  -- Otherwise move both up one parent
+  undefined
+
+-- This is all inefficient and we should build a better data structure...  But this is easier!
+findElementById :: String -> OrbitElement -> Maybe OrbitElement
+findElementById id root = do
+  -- Is it the map root?
+  if id == (elem_id root)
+  then Just root
+  else do
+    -- Is it in the root's children?
+    let matching_child = listToMaybe $ filter (\e -> id == elem_id e) (children root)
+    case matching_child of
+      Just x -> Just x
+      -- No match in children - recurse.  Hopefully this will be lazy!
+      Nothing -> listToMaybe $ catMaybes $ map (findElementById id) (children root)
+
+findParentForId :: String -> OrbitElement -> Maybe OrbitElement
+findParentForId id root = do
+  -- Is it in the root's children?
+  let matching_child = listToMaybe $ filter (\e -> id == elem_id e) (children root)
+  case matching_child of
+    Just x -> Just root
+    -- No match in children - recurse.  Hopefully this will be lazy!
+    Nothing -> listToMaybe $ catMaybes $ map (findParentForId id) (children root)
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -7,30 +7,30 @@ test_day6 :: IO ()
 test_day6 = hspec $ do
   describe "constructMap" $ do
     it "Empty data" $ do
-      constructMap "" `shouldBe` OrbitElement "COM" []
+      constructMap "" `shouldBe` OrbitElement "COM" [] 0
 
     it "Single orbit" $ do
       constructMap "COM)123" `shouldBe` (
         OrbitElement "COM" [
-            OrbitElement "123" []
-            ]
+            OrbitElement "123" [] 1
+            ] 0
         )
 
     it "Two children" $ do
       constructMap "COM)123\nCOM)321" `shouldBe` (
         OrbitElement "COM" [
-            OrbitElement "123" [],
-            OrbitElement "321" []
-            ]
+            OrbitElement "123" [] 1,
+            OrbitElement "321" [] 1
+            ] 0
         )
 
     it "Two deep" $ do
       constructMap "COM)123\n123)321" `shouldBe` (
         OrbitElement "COM" [
             OrbitElement "123" [
-                OrbitElement "321" []
-                ]
-            ]
+                OrbitElement "321" [] 2
+                ] 1
+            ] 0
         )
 
     it "Complex" $ do
@@ -38,12 +38,23 @@ test_day6 = hspec $ do
         OrbitElement "COM" [
             OrbitElement "123" [
                 OrbitElement "321" [
-                    OrbitElement "444" []
-                    ],
-                OrbitElement "TTR" []
-                ]
-            ]
+                    OrbitElement "444" [] 3
+                    ] 2,
+                OrbitElement "TTR" [] 2
+                ] 1
+            ] 0
         )
+
+  describe "countOrbits" $ do
+    it "No children" $ do
+      countOrbits (OrbitElement "COM" [] 0) `shouldBe` 0
+      countOrbits (OrbitElement "432" [] 1) `shouldBe` 1
+
+    it "One child" $ do
+      countOrbits (constructMap "COM)123") `shouldBe` 1
+
+    it "Website example" $ do
+      countOrbits (constructMap "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") `shouldBe` 42
 
   describe "isInstructionFor" $ do
     it "Parent match" $ do
@@ -53,3 +64,53 @@ test_day6 = hspec $ do
     it "Child match" $ do
       isInstructionFor "123" ("COM", "123") `shouldBe` False
       isInstructionFor "COM" ("123", "COM") `shouldBe` False
+
+  describe "findElementById" $ do
+    it "COM only - matches" $ do
+      findElementById "COM" (constructMap "") `shouldBe` Just (OrbitElement "COM" [] 0)
+
+    it "COM only - non-match" $ do
+      findElementById "MOC" (constructMap "") `shouldBe` Nothing
+
+    it "one orbit, match COM" $ do
+      findElementById "COM" (constructMap "COM)123") `shouldBe` Just (OrbitElement "COM" [
+                                                                         OrbitElement "123" [] 1
+                                                                         ] 0)
+
+    it "one orbit, match child" $ do
+      findElementById "123" (constructMap "COM)123") `shouldBe` Just (OrbitElement "123" [] 1)
+
+    it "one orbit, non-match" $ do
+      findElementById "321" (constructMap "COM)123") `shouldBe` Nothing
+
+    it "Website example, match" $ do
+      findElementById "K" (constructMap "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") `shouldBe` Just (
+        OrbitElement "K" [
+            OrbitElement "L" [] 7
+            ] 6
+        )
+
+    it "Website example, non-match" $ do
+      findElementById "Z" (constructMap "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") `shouldBe` Nothing
+
+  describe "findParentForId" $ do
+    it "COM only" $ do
+      findParentForId "COM" (constructMap "") `shouldBe` Nothing
+
+    it "one orbit, match" $ do
+      findParentForId "123" (constructMap "COM)123") `shouldBe` Just (OrbitElement "COM" [
+                                                                         OrbitElement "123" [] 1
+                                                                         ] 0)
+
+    it "one orbit, non-match" $ do
+      findParentForId "443" (constructMap "COM)123") `shouldBe` Nothing
+
+    it "Website example, match" $ do
+      findParentForId "L" (constructMap "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") `shouldBe` Just (
+        OrbitElement "K" [
+            OrbitElement "L" [] 7
+            ] 6
+        )
+
+    it "Website example, non-match" $ do
+      findParentForId "Z" (constructMap "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L") `shouldBe` Nothing
