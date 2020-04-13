9e58399e0e90a98434c1db5aa089048feebad065
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-13 13:34:05 +0100

    Basic map parsing and test

--- src/Day6.hs
+++ src/Day6.hs
@@ -9,4 +9,33 @@ day6 :: IO ()
 day6 = do
   putStrLn "day6 start"
   contents <- readFile "data/day6"
+  let map = constructMap contents
+  print (show map)
   putStrLn "day6 end"
+
+type OrbitMap = OrbitElement
+data OrbitElement = OrbitElement { id :: String
+                                 -- , parent :: Maybe OrbitElement -- TODO Not implemented, not sure if we'll need it
+                                 , children :: [OrbitElement]
+                                 } deriving (Show, Eq)
+type OrbitInstruction = (String, String) -- (parent, child)
+
+constructMap :: String -> OrbitMap
+constructMap s = do
+  -- Split string into lines
+  let ls = lines s
+  -- let pairs = [(splitOn ")" l) | l <- ls]
+  let pairs = map (splitOn ")") ls
+  let ins = [(p!!0, p!!1) | p <- pairs]
+  OrbitElement "COM" (getOrbitChildren "COM" ins)
+
+getOrbitChildren :: String -> [OrbitInstruction] -> [OrbitElement]
+getOrbitChildren id ins = do
+  let direct = filter (isInstructionFor id) ins
+  [ OrbitElement child_id (getOrbitChildren child_id ins)
+    | (_, child_id) <- direct ]
+
+isInstructionFor :: String -> OrbitInstruction -> Bool
+isInstructionFor id ins = do
+  let (parent_id, _) = ins
+  parent_id == id
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -5,6 +5,51 @@ import Day6
 
 test_day6 :: IO ()
 test_day6 = hspec $ do
-  describe "placeholder" $ do
-    it "1 == 1" $ do
-      1 `shouldBe` 1
+  describe "constructMap" $ do
+    it "Empty data" $ do
+      constructMap "" `shouldBe` OrbitElement "COM" []
+
+    it "Single orbit" $ do
+      constructMap "COM)123" `shouldBe` (
+        OrbitElement "COM" [
+            OrbitElement "123" []
+            ]
+        )
+
+    it "Two children" $ do
+      constructMap "COM)123\nCOM)321" `shouldBe` (
+        OrbitElement "COM" [
+            OrbitElement "123" [],
+            OrbitElement "321" []
+            ]
+        )
+
+    it "Two deep" $ do
+      constructMap "COM)123\n123)321" `shouldBe` (
+        OrbitElement "COM" [
+            OrbitElement "123" [
+                OrbitElement "321" []
+                ]
+            ]
+        )
+
+    it "Complex" $ do
+      constructMap "321)444\nCOM)123\n123)321\n123)TTR" `shouldBe` (
+        OrbitElement "COM" [
+            OrbitElement "123" [
+                OrbitElement "321" [
+                    OrbitElement "444" []
+                    ],
+                OrbitElement "TTR" []
+                ]
+            ]
+        )
+
+  describe "isInstructionFor" $ do
+    it "Parent match" $ do
+      isInstructionFor "COM" ("COM", "123") `shouldBe` True
+      isInstructionFor "123" ("123", "COM") `shouldBe` True
+
+    it "Child match" $ do
+      isInstructionFor "123" ("COM", "123") `shouldBe` False
+      isInstructionFor "COM" ("123", "COM") `shouldBe` False
