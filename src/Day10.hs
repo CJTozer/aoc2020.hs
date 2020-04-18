ba63890b1d6be0a531ada6169909d7e44f2352fc
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-18 14:14:59 +0100

    Code and tests for counting visible asteroids from point

--- src/Day10.hs
+++ src/Day10.hs
@@ -2,6 +2,9 @@
 
 module Day10 (
   day10,
+  countVisibleFrom,
+  hasLineOfSight,
+  directlyBetween,
   coLinear
   ) where
 
@@ -17,17 +20,34 @@ day10 = do
   contents <- readFile "data/day10"
   putStrLn "day10 end"
 
+countVisibleFrom :: [Point] -> Point -> Int
+countVisibleFrom asteroids p = accVisible asteroids asteroids p
+
+accVisible :: [Point] -> [Point] -> Point -> Int
+accVisible [] _ _ = 0
+accVisible (t:ts) blockers p = (if hasLineOfSight blockers t p then 1 else 0) + accVisible ts blockers p
+
 hasLineOfSight :: [Point] -> Point -> Point -> Bool
 hasLineOfSight [] _ _ = True
 hasLineOfSight (b:bs) p1 p2 = (not $ directlyBetween b p1 p2) && hasLineOfSight bs p1 p2
 
 directlyBetween :: Point -> Point -> Point -> Bool
-directlyBetween t p1 p2 = do
-  -- False if it == p1 or p2
+directlyBetween (tx, ty) (x1, y1) (x2, y2) = and [
+  -- False if t == p1 or p2
+  not ((tx, ty) == (x1, y1)),
+  not ((tx, ty) == (x2, y2)),
   -- t.x between p1.x and p2.x
+  (between tx x1 x2),
   -- t.y between p1.y and p2.y
+  (between ty y1 y2),
   -- t, p1, p2 are colinear
-  False
+  (coLinear (tx, ty) (x1, y1) (x2, y2))
+  ]
+
+-- Inclusive between returns (x1 <= x <= x2) - or v1/2 the other way around
+between :: Int -> Int -> Int -> Bool
+between x v1 v2 =
+  x >= min v1 v2 && x <= max v1 v2
 
 coLinear :: Point -> Point -> Point -> Bool
 coLinear (x1, y1) (x2, y2) (x3, y3) = do
