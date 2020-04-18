7e4995aebf5329915493352f9de1e1a7519d9f57
Chris Tozer <chris.tozer@metaswitch.com>
2020-04-18 13:36:17 +0100

    Day 10 tests for coLinear points

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,12 +1,43 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Day10 (day10) where
+module Day10 (
+  day10,
+  coLinear
+  ) where
 
-import Data.List.Split
+import Data.List
+import Data.Ratio
 import Debug.Trace
+
+type Point = (Int, Int)
 
 day10 :: IO ()
 day10 = do
   putStrLn "day10 start"
   contents <- readFile "data/day10"
   putStrLn "day10 end"
+
+hasLineOfSight :: [Point] -> Point -> Point -> Bool
+hasLineOfSight [] _ _ = True
+hasLineOfSight (b:bs) p1 p2 = (not $ directlyBetween b p1 p2) && hasLineOfSight bs p1 p2
+
+directlyBetween :: Point -> Point -> Point -> Bool
+directlyBetween t p1 p2 = do
+  -- False if it == p1 or p2
+  -- t.x between p1.x and p2.x
+  -- t.y between p1.y and p2.y
+  -- t, p1, p2 are colinear
+  False
+
+coLinear :: Point -> Point -> Point -> Bool
+coLinear (x1, y1) (x2, y2) (x3, y3) = do
+  -- co-linear if the vectors p1-p2 and p1-p3 have the same direction (or -ve)
+  -- Represent direction as x:y ratio for vector, which means we need to check for potential y=0 first
+  let v1 = getDirection (x1 - x2) (y1 - y2)
+  let v2 = getDirection (x1 - x3) (y1 - y3)
+  v1 == v2
+
+getDirection :: Int -> Int -> Maybe (Ratio Int)
+getDirection x y = case y of
+  0 -> Nothing -- Nothing represents the horizontal vector
+  _ -> Just $ x % y -- Otherwise the x:y ratio represents the direction
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -1,10 +1,31 @@
 module Day10Spec where
 
+import Data.List
 import Test.Hspec
 import Day10
 
+testAllCoLinear a b c res = do
+  coLinear a b c `shouldBe` res
+  coLinear a c b `shouldBe` res
+  coLinear b a c `shouldBe` res
+  coLinear b c a `shouldBe` res
+  coLinear c a b `shouldBe` res
+  coLinear c b a `shouldBe` res
+
 test_day10 :: IO ()
 test_day10 = hspec $ do
-  describe "placeholder" $ do
-    it "1 == 1" $ do
-      1 `shouldBe` 1
+  describe "coLinear" $ do
+    it "y = x" $ do
+      testAllCoLinear (0,0) (1,1) (2,2) True
+
+    it "y = 2 - x" $ do
+      testAllCoLinear (0,2) (1,1) (2,0) True
+
+    it "Not on line with a matching y" $ do
+      testAllCoLinear (0,0) (1,2) (2,2) False
+
+    it "Not on line with a matching x" $ do
+      testAllCoLinear (0,0) (1,2) (2,2) False
+
+    it "In line, but non-trivial ratios" $ do
+      testAllCoLinear (2,-4) (11,11) (-4,-14) True
