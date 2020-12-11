2826e5c0c91d588db14b1a47268cc56855abee62
Chris Tozer <chris.tozer@metaswitch.com>
2020-12-11 04:00:04 +0000

    Day 11 part 2

--- src/Day11.hs
+++ src/Day11.hs
@@ -40,7 +40,7 @@ newState :: Pos -> [String] -> Char
 newState p@(x, y) s =
   case this_seat of
     '.' -> '.'
-    '#' -> if occupied_neighbours >= 4
+    '#' -> if occupied_neighbours >= 5
       then 'L'
       else '#'
     'L' -> if occupied_neighbours == 0
@@ -50,16 +50,16 @@ newState p@(x, y) s =
     this_seat = seatAtPos p s
     occupied_neighbours = countOccupiedNeighbours p s
 
--- Safe - anything out-of-bounds is floor
+-- Safe - anything out-of-bounds is (a special char representing) floor
 seatAtPos :: Pos -> [String] -> Char
 seatAtPos (x, y) s =
   if (x < 0) || (x >= length s) || (y < 0) || (y >= length (head s))
-  then '.'
+  then ','
   else (s !! x) !! y
 
 countOccupiedNeighbours :: Pos -> [String] -> Int
 countOccupiedNeighbours p s =
-  sum . (map isOccupied) $ getNeighbours p s
+  sum . (map isOccupied) $ getNeighbours' p s
 
 -- 0 for unoccupied, 1 for occupied
 isOccupied :: Char -> Int
@@ -71,3 +71,20 @@ getNeighbours (x, y) s =
    x' <- [x-1, x, x+1],
    y' <- [y-1, y, y+1],
    (x' /= x) || (y' /= y)]
+
+getNeighbours' :: Pos -> [String] -> [Char]
+getNeighbours' p s =
+  [getNeighbourOnVector p (vx, vy) s |
+   vx <- [-1, 0, 1],
+   vy <- [-1, 0, 1],
+   (vx /= 0) || (vy /= 0)]
+
+getNeighbourOnVector :: Pos -> Pos -> [String] -> Char
+getNeighbourOnVector (px, py) v@(vx, vy) s =
+  case seat of
+    -- Keep going if floor, otherwise return the value found
+    '.' -> getNeighbourOnVector p' v s
+    x -> x
+  where
+    p' = (px + vx, py + vy)
+    seat = seatAtPos p' s
