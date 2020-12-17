6db4ede45fd5260d826f988e0a1aa46bdf659944
Chris Tozer <chris.tozer@metaswitch.com>
2020-12-17 09:53:59 +0000

    Day 17 part 1 with a Set instead

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,16 +1,14 @@
-{-# LANGUAGE ParallelListComp #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Day17 (day17) where
 
 import Data.List.Split
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Debug.Trace
 
 type Pos = (Int, Int, Int)
-data Cell = Inactive | Active deriving (Show)
-type State = Map Pos Cell
+type State = Set Pos
 
 day17 :: IO ()
 day17 = do
@@ -32,12 +30,9 @@ getActiveCells :: Int -> [String] -> [Pos]
 getActiveCells z ls =
   [ (x, y, z)
   | y <- [0 .. length (head ls) -1]
-  | x <- [0 .. length ls - 1]
-  -- TODO only get the active ones!
+  , x <- [0 .. length ls - 1]
+  , (ls !! x) !! y == '#'
   ]
 
 stateFromActiveCells :: [Pos] -> State
-stateFromActiveCells = foldr (`Map.insert` Active) Map.empty
-
-getCell :: Pos -> State -> Cell
-getCell = Map.findWithDefault Inactive
+stateFromActiveCells = Set.fromList
