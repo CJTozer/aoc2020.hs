0491e1838e1602dce14915d346ec7cd262a7a7ac
Chris Tozer <chris.tozer@metaswitch.com>
2021-01-04 16:18:41 +0000

    Attempted day 23 Array-based method; no faster and hits memory allocation issues

--- src/Day23.hs
+++ src/Day23.hs
@@ -2,6 +2,8 @@
 
 module Day23 (day23) where
 
+import Data.Array.Unboxed (Array)
+import qualified Data.Array.Unboxed as Array
 import Data.IntMap.Strict (IntMap)
 import qualified Data.IntMap.Strict as IntMap
 import Data.List (elemIndex)
@@ -11,9 +13,13 @@ import Debug.Trace
 day23 :: IO ()
 day23 = do
   putStrLn "day23 start"
-  part1
   part2
+  part2'
   putStrLn "day23 end"
+
+part2N = 20
+
+part2Limit = 1000000
 
 -- Naive list-based implementation for day 1
 part1 :: IO ()
@@ -57,8 +63,8 @@ insertDest current taken =
   if prev `notElem` taken
     then prev
     else insertDest prev taken
- where
-  prev = prevLabel current
+  where
+    prev = prevLabel current
 
 prevLabel :: Int -> Int
 prevLabel x = if x == 1 then 9 else x - 1
@@ -73,12 +79,12 @@ insertAfter wedge target list = do
 part2 :: IO ()
 part2 = do
   let m = startingMap
-  let n = 10000000
-  print . show . takeNFromMap 10 1 . doNMoves' n $ m
+  let n = part2N
+  print . show . takeNFromMap 30 1 . doNMoves' n $ m
   print . show . product . takeNFromMap 3 1 . doNMoves' n $ m
 
 startingPos' :: [Int]
-startingPos' = startingPos ++ [10 .. 1000000]
+startingPos' = startingPos ++ [10 .. part2Limit]
 
 startingMap :: IntMap Int
 startingMap =
@@ -108,7 +114,7 @@ doMove' m = do
   -- Plonk the 3 taken in after this
   let m'' = replaceTakenAfter taken target m'
   -- Update the new current cup
-  setinsertDest m'' current
+  setInsertDest m'' current
 
 currentCup :: IntMap Int -> Int
 currentCup m = m IntMap.! 0
@@ -118,14 +124,14 @@ nextCup m x = m IntMap.! x
 
 removeTaken :: IntMap Int -> IntMap Int
 removeTaken m = IntMap.insert (currentCup m) fourth_cup m
- where
-  fourth_cup = nextCup m . nextCup m . nextCup m . nextCup m $ currentCup m
+  where
+    fourth_cup = nextCup m . nextCup m . nextCup m . nextCup m $ currentCup m
 
 collectThreeAfter :: IntMap Int -> Int -> [Int]
 collectThreeAfter m x =
-  [ nextCup m x
-  , nextCup m . nextCup m $ x
-  , nextCup m . nextCup m . nextCup m $ x
+  [ nextCup m x,
+    nextCup m . nextCup m $ x,
+    nextCup m . nextCup m . nextCup m $ x
   ]
 
 replaceTakenAfter :: [Int] -> Int -> IntMap Int -> IntMap Int
@@ -142,17 +148,86 @@ insertDest' current taken =
   if prev `notElem` taken
     then prev
     else insertDest' prev taken
- where
-  prev = prevLabel' current
+  where
+    prev = prevLabel' current
 
 prevLabel' :: Int -> Int
-prevLabel' x = if x == 1 then 1000000 else x - 1
+prevLabel' x = if x == 1 then part2Limit else x - 1
 
-setinsertDest :: IntMap Int -> Int -> IntMap Int
-setinsertDest m current = IntMap.insert 0 (nextCup m current) m
+setInsertDest :: IntMap Int -> Int -> IntMap Int
+setInsertDest m current = IntMap.insert 0 (nextCup m current) m
 
 takeNFromMap :: Int -> Int -> IntMap Int -> [Int]
 takeNFromMap 1 from _ = [from]
 takeNFromMap n from m = do
   let next = m IntMap.! from
   from : takeNFromMap (n - 1) next m
+
+-- Try an Array-based alternative
+-- Does too much memory allocation, even with unboxed arrays
+part2' :: IO ()
+part2' = do
+  let a = startingArray
+  let n = part2N
+  print . show . takeNFromArray 30 1 . doNMoves'' n $ a
+  print . show . product . takeNFromArray 3 1 . doNMoves'' n $ a
+
+type ArrayUpdateOp = (Int, Int)
+
+startingArray :: Array Int Int
+startingArray =
+  -- And add 0 to the start to indicate the current cup is the first cup
+  Array.array (0, length startingPos') links
+  where
+    links = zip ixs elems
+    ixs = 0 : startingPos'
+    elems = startingPos' ++ take 1 startingPos'
+
+doNMoves'' :: Int -> Array Int Int -> Array Int Int
+doNMoves'' 0 a = a
+doNMoves'' n a = doNMoves'' (n - 1) $ doMove'' a
+
+doMove'' :: Array Int Int -> Array Int Int
+doMove'' a = do
+  -- Get current cup
+  let current = currentCup' a
+  -- "Take" next 3 cups
+  let taken = collectThreeAfter' a current
+  -- Gather the operations we need to do - we'll do them all at once for space efficiency
+  let ops = removeTaken' a
+  -- Choose destination cup: highest below current that's not in taken
+  let target = insertDest' current taken
+  -- Plonk the 3 taken in after this
+  let ops' = replaceTakenAfter' taken target a
+  -- Update the array
+  a Array.// (ops ++ ops')
+
+currentCup' :: Array Int Int -> Int
+currentCup' a = a Array.! 0
+
+nextCup' :: Array Int Int -> Int -> Int
+nextCup' a x = a Array.! x
+
+collectThreeAfter' :: Array Int Int -> Int -> [Int]
+collectThreeAfter' a x =
+  [ nextCup' a x,
+    nextCup' a . nextCup' a $ x,
+    nextCup' a . nextCup' a . nextCup' a $ x
+  ]
+
+-- Next cup will always be the fourth cup
+removeTaken' :: Array Int Int -> [ArrayUpdateOp]
+removeTaken' a = [(currentCup' a, fourth_cup), (0, fourth_cup)]
+  where
+    fourth_cup = nextCup' a . nextCup' a . nextCup' a . nextCup' a $ currentCup' a
+
+replaceTakenAfter' :: [Int] -> Int -> Array Int Int -> [ArrayUpdateOp]
+replaceTakenAfter' taken target a =
+  -- Target now points to first taken
+  [(target, head taken), (last taken, nextCup' a target)]
+
+takeNFromArray :: Int -> Int -> Array Int Int -> [Int]
+takeNFromArray 1 from _ = [from]
+takeNFromArray n from a = do
+  let next = a Array.! from
+  from : takeNFromArray (n - 1) next a
